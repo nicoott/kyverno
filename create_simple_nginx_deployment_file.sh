9be33ee8a55eb23ef6c23538cd69af6d05b04eba
nicoott <nico.ott@outlook.com>
2024-10-08 17:25:21 +0200

    Added nginx-deploymemt creator

---
+++ create_simple_nginx_deployment_file.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+DEPLOYMENT_NAME="nginx-test"
+IMAGE="nginx"
+FILE_NAME="nginx-deployment.yaml"
+kubectl create --image="${IMAGE}" "${DEPLOYMENT_NAME}" --dry-run=client -o yaml > "${FILE_NAME}"
